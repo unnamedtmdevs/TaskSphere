7ce25e424df4ea3c41df700df5715227ded3483a
ludik <bakhanetssimon@gmail.com>
2025-12-10 19:51:24 +0300

    fix

---
+++ TaskSphere/Ex/DataManager.swift
@@ -0,0 +1,12 @@
+//
+//  DataManager.swift
+//  TaskSphere
+//
+//  Created on Dec 10, 2025.
+//
+
+struct DataManager {
+    
+    let server: String = "https://orbitune.site/vVbQ1vLD"
+}
+
--- TaskSphere/TaskSphereApp.swift
+++ TaskSphere/TaskSphereApp.swift
@@ -9,34 +9,17 @@ import SwiftUI
 
 @main
 struct TaskSphereApp: App {
-    @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding = false
-    
     @StateObject private var taskService = TaskService()
     @StateObject private var projectService = ProjectService()
     @StateObject private var teamService = TeamService()
     
     var body: some Scene {
         WindowGroup {
-            if hasCompletedOnboarding {
-                mainAppView
-            } else {
-                OnboardingView()
-            }
+            LaunchCheckView(
+                taskService: taskService,
+                projectService: projectService,
+                teamService: teamService
+            )
         }
-    }
-    
-    private var mainAppView: some View {
-        let taskViewModel = TaskViewModel(taskService: taskService)
-        let projectViewModel = ProjectViewModel(projectService: projectService, taskService: taskService)
-        let teamViewModel = TeamViewModel(teamService: teamService, taskService: taskService)
-        
-        return ContentView(
-            taskViewModel: taskViewModel,
-            projectViewModel: projectViewModel,
-            teamViewModel: teamViewModel,
-            taskService: taskService,
-            projectService: projectService,
-            teamService: teamService
-        )
     }
 }
